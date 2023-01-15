6ef90cc5597b6e4cacdeef94277a6de7d66c0fe5
Lamine Diallo <bah07565@gmail.com>
2023-01-15 22:06:39 +0100

    adding styling

--- view/layout.html.php
+++ view/layout.html.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"> -->
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
 </head>
 
 <body>
@@ -15,22 +17,14 @@
 
         <h1 class="mb-4">Todo List</h1>
 
-        <div class="btn-group mb-4" role="group">
-            <a href="./" type="button" class="btn btn-outline-primary active" data-filter="all">Toutes</a>
-            <a href="index.php?task=getActive" type="button" class="btn btn-outline-primary" data-filter="todo">A faire</a>
-            <a href="index.php?task=getInactive" type="button" class="btn btn-outline-primary" data-filter="done">Faites</a>
-        </div>
+        <form class="d-flex pb-4" action="index.php?task=add" method="post">
+            <input required class="mr-4 form-control" type="text" placeholder="Acheter des patates..." name="content" data-com.bitwarden.browser.user-edited="yes">
+            <button type="submit" class="ms-2 btn btn-primary">Ajouter</button>
+        </form>
 
         <main>
 
-            <form class="d-flex pb-4" action="index.php?task=add" method="post">
-                <input required class="mr-4 form-control" type="text" placeholder="Acheter des patates..." name="content" data-com.bitwarden.browser.user-edited="yes">
-                <button type="submit" class="ms-2 btn btn-primary">Ajouter</button>
-            </form>
-
-            <ul class="list-group">
-                <?= $pageContent ?>
-            </ul>
+            <?= $pageContent ?>
 
         </main>
 
--- view/tasks/active.html.php
+++ view/tasks/active.html.php
@@ -1,14 +1,28 @@
-<?php foreach ($tasks as $task) { ?>
-    <li class="todo list-group-item d-flex align-items-center">
-        <input class="form-check-input" type="checkbox" id="todo" <?= $task->status === 1 ? "checked" : null ?> />
-        <label class="ms-2 form-check-label" for="todo">
-            <?= $task->content ?>
-        </label>
-        <label class="ms-auto btn btn-danger btn-sm">
-            <a href="" onclick=confirme()>
-                <i class="bi-trash text-white"></i>
-            </a>
-        </label>
-    </li>
+<div class="btn-group mb-4" id="group" role="group">
+    <a href="./" type="button" class="btn btn-outline-primary">Toutes</a>
+    <a href="index.php?task=getInactive" type="button" class="btn btn-outline-primary">A faire</a>
+    <a href="index.php?task=getActive" type="button" class="btn btn-outline-primary active">Faites</a>
+</div>
 
-<?php } ?>
+<ul class="list-group">
+    <?php foreach ($tasks as $task) { ?>
+
+        <li class="todo list-group-item d-flex align-items-center">
+
+            <a href="">
+                <input class="form-check-input" type="checkbox" <?= $task->status === 1 ? "checked" : null ?>>
+                <label class="ms-2 form-check-label" id="content">
+                    <?= $task->content ?>
+                </label>
+            </a>
+
+            <label class="ms-auto btn btn-danger btn-sm">
+                <a href="" onclick=confirme()>
+                    <img src="style/poubelle.svg" alt="">
+                </a>
+            </label>
+
+        </li>
+
+    <?php } ?>
+</ul>
